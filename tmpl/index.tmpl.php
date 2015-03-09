66dac59641a4cebc9a81b76f773a865bd10d5e44
germannparisi <germannparisi@f8032c49-8668-b9d0-1405-d9e036d7ce0a>
2015-03-09 03:37:15 +0000

    Commit inicial!

---
+++ tmpl/index.tmpl.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+	<head>
+		<title>Base de datos de Conocimiento</title>
+	</head>
+	<body>
+		<h1>Mis conocimientos</h1>
+		<form action="buscar.php" method="POST">
+			<input type="text" id="txtBuscar" name="txtBuscar" />
+			<input type="submit" id="btnBuscar" name="btnBuscar" value="Buscar" />
+		</form>
+		<div style="margin: 10px;">
+			<a href="registrar.php">Registrar</a>
+		</div>
+	</body>
+</html>
---
+++ tmpl/registrar.tmpl.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Administrador de contenido</title>
+	</head>
+	<body>
+		<h1>Mis contenidos</h1>
+		<form action="registrar.php" method="post" >
+			<table>
+				<tr>
+					<td>
+						<label>Clave: </label>
+					</td>
+					<td>
+						<input type="text" id="txtClave" name="txtClave" style="width: 100%" />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Título: </label>
+					</td>
+					<td>
+						<input type="text" id="txtTitulo" name="txtTitulo" style="width: 100%" />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Tipo: </label>
+					</td>
+					<td>
+						<select>
+							<option value="lib">Libro</option>
+							<option value="lib_pdf">Libro PDF</option>
+							<option value="url">URL</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Descripción: </label>
+					</td>
+					<td>
+						<textarea id="txaDescripcion" name="txaDescripcion" rows="15" cols="70"> </textarea>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<label>Calidad</label>
+					</td>
+					<td>
+						<select>
+							<option value="exc">Excelente</option>
+							<option value="bue">Buena</option>
+							<option value="reg">Regular</option>
+							<option value="mal">Mala</option>
+							<option value="no_">No sé</option>
+							<option value="pro">Prometedor</option>
+						</select>
+					</td>
+				</tr>
+			</table>
+			<hr />
+			<div>
+				<input type="submit" id="btnRegistrar" name="btnRegistrar" value="Registrar" />
+			</div>
+		</form>
+	</body>
+</html>
