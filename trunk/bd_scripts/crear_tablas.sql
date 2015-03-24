d96bf57acff2a23d809de893f88fb36853a754bf
germannparisi <germannparisi@f8032c49-8668-b9d0-1405-d9e036d7ce0a>
2015-03-24 03:58:38 +0000

    Agrego obtencion de calidades.

--- trunk/bd_scripts/crear_tablas.sql
+++ trunk/bd_scripts/crear_tablas.sql
@@ -4,13 +4,19 @@ USE conocimiento;
 CREATE TABLE tipos_materiales(
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR(30),
-	PRIMARY KEY (id)
+	PRIMARY KEY(id)
+);
+CREATE TABLE calidades(
+    id INT AUTO_INCREMENT,
+    nombre VARCHAR(30),
+    PRIMARY KEY(id)
 );
 CREATE TABLE materiales(
 	id INT AUTO_INCREMENT,
-	clave VARCHAR(256) NOT NULL,
+	fuente VARCHAR(256) NOT NULL,
 	titulo VARCHAR(100) NOT NULL,
 	id_tipo_material INT NOT NULL,
-	url_descripcion VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
+	url_descripcion VARCHAR(128) NOT NULL,
+	descripcion TEXT NOT NULL,
+	PRIMARY KEY(id)
 );
--- trunk/bd_scripts/insertar_datos.sql
+++ trunk/bd_scripts/insertar_datos.sql
@@ -6,3 +6,11 @@ INSERT INTO tipos_materiales(nombre) VALUES ('Libro impreso');
 INSERT INTO tipos_materiales(nombre) VALUES ('Libro fotocopiado');
 INSERT INTO tipos_materiales(nombre) VALUES ('URL');
 INSERT INTO tipos_materiales(nombre) VALUES ('URL Wikipedia');
+
+INSERT INTO calidades(nombre) VALUES ('Excelente');
+INSERT INTO calidades(nombre) VALUES ('Buena');
+INSERT INTO calidades(nombre) VALUES ('Regular');
+INSERT INTO calidades(nombre) VALUES ('Mala');
+INSERT INTO calidades(nombre) VALUES ('Desconocida');
+INSERT INTO calidades(nombre) VALUES ('Prometedor');
+
