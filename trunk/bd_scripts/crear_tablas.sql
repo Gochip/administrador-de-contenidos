da2fefc8593dba572fc05fc78fa3c8062c60ecef
germannparisi <germannparisi@f8032c49-8668-b9d0-1405-d9e036d7ce0a>
2015-03-24 19:07:11 +0000

    Finalizo insercion de datos. Hago busqueda por base de datos.

--- trunk/bd_scripts/crear_tablas.sql
+++ trunk/bd_scripts/crear_tablas.sql
@@ -5,18 +5,20 @@ CREATE TABLE tipos_materiales(
 	id INT AUTO_INCREMENT,
 	nombre VARCHAR(30),
 	PRIMARY KEY(id)
-);
+)ENGINE InnoDb;
 CREATE TABLE calidades(
     id INT AUTO_INCREMENT,
     nombre VARCHAR(30),
     PRIMARY KEY(id)
-);
+)ENGINE InnoDb;
 CREATE TABLE materiales(
 	id INT AUTO_INCREMENT,
 	fuente VARCHAR(256) NOT NULL,
 	titulo VARCHAR(100) NOT NULL,
 	id_tipo_material INT NOT NULL,
+	id_calidad INT NOT NULL,
 	url_descripcion VARCHAR(128) NOT NULL,
 	descripcion TEXT NOT NULL,
-	PRIMARY KEY(id)
-);
+	PRIMARY KEY(id),
+	UNIQUE(fuente)
+)ENGINE InnoDb;
