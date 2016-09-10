95b33340b499f07c55b9e944ec2b203e08a0d5ef
Gochi <germannparisi@gmail.com>
2016-09-10 15:13:21 -0300

    Agrego video como tipo de material

--- bd_scripts/evolucion/alter_2.sql
+++ bd_scripts/evolucion/alter_2.sql
@@ -1,3 +1,4 @@
 INSERT INTO tipos_materiales(nombre) VALUES ('Artículo');
 INSERT INTO tipos_materiales(nombre) VALUES ('Paper');
 INSERT INTO tipos_materiales(nombre) VALUES ('URL Github');
+INSERT INTO tipos_materiales(nombre) VALUES ('Video');
