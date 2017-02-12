e82bebeb01e6a9b41c06caf88a8e7c5bee7b333e
Gochi <germannparisi@gmail.com>
2017-02-12 16:56:27 -0300

    Agrego contenedores de contenidos y cambios en los tipos de materiales

---
+++ bd_scripts/evolucion/alter_3.sql
@@ -0,0 +1,15 @@
+UPDATE calidades SET nombre='No visto' WHERE nombre='Prometedor' AND id>0;
+
+INSERT INTO tipos_materiales(nombre) VALUES ('Curso online');
+INSERT INTO tipos_materiales(nombre) VALUES ('Herramienta web');
+
+CREATE TABLE contenedores(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255)
+);
+
+INSERT INTO contenedores(nombre) VALUES('Informática');
+
+ALTER TABLE materiales ADD COLUMN id_contenedor INT;
+
+UPDATE materiales SET id_contenedor=1 WHERE id>0;
